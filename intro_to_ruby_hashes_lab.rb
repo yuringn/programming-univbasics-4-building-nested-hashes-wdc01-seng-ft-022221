ec11adc19054d98c218c8fadfe782b3b9c8b1490
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 03:52:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ base_hash
 
 def monopoly_with_second_tier
   monopoly={:railroads=>{},
-            :pieces => 4,
+            :pieces=> 4,
             :rent_in_dollars=>{},
             :names=>{}
 
