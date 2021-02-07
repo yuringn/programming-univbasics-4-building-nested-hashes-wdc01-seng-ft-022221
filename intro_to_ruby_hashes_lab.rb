523fa0e207292d5cecdc0ed9caa6a3508b60839d
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 03:47:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-{name:"yuri"}
+monopoly={:railroads=>{}}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
@@ -7,6 +7,7 @@ end
 base_hash
 
 def monopoly_with_second_tier
+  monopoly={:railroads=>{}}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
